a821940396b3f5869a52c18bf775695aae52489c
Jesse Wolgamott <jesse@comalproductions.com>
2016-04-07 11:47:30 -0500

    Try moving the config away from initializers

--- app/channels/cart_channel.rb
+++ app/channels/cart_channel.rb
@@ -11,3 +11,7 @@ class CartChannel < ApplicationCable::Channel
   def update
   end
 end
+
+if Rails.env.production?
+  Rails.application.config.action_cable.allowed_request_origins = ['https://inventory-cable-example-2016.herokuapp.com/']
+end
--- config/initializers/cable.rb
+++ config/initializers/cable.rb
@@ -1,3 +1,3 @@
-if Rails.env.production?
-  Rails.application.config.action_cable.allowed_request_origins = ['https://inventory-cable-example-2016.herokuapp.com/']
-end
+# if Rails.env.production?
+#   Rails.application.config.action_cable.allowed_request_origins = ['https://inventory-cable-example-2016.herokuapp.com/']
+# end
